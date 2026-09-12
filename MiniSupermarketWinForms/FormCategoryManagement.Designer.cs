888421285ff078333b05b89ced697a7c077ff096
Nguyen Ngoc Minh Thu <minhthunguyenngoc280@gmail.com>
2026-09-12 22:02:38 +0700

    Hoàn thành bài thực hành Buổi 1: CRUD Nhóm hàng MiniSupermarket

---
+++ MiniSupermarketWinForms/FormCategoryManagement.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace MiniSupermarketWinForms
+{
+    partial class FormCategoryManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvCategories = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategories)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tìm kiém";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(759, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(59, 42);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(228, 20);
+            this.txtKeyword.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(299, 42);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(380, 42);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Tải lại\r\n";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvCategories);
+            this.groupBox1.Location = new System.Drawing.Point(59, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(396, 280);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách nhóm hàng";
+            // 
+            // dgvCategories
+            // 
+            this.dgvCategories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategories.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCategories.Location = new System.Drawing.Point(3, 16);
+            this.dgvCategories.Name = "dgvCategories";
+            this.dgvCategories.Size = new System.Drawing.Size(390, 261);
+            this.dgvCategories.TabIndex = 0;
+            this.dgvCategories.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCategories_CellClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.txtDescription);
+            this.groupBox2.Controls.Add(this.txtCategoryName);
+            this.groupBox2.Controls.Add(this.txtId);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(461, 94);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(269, 280);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin Nhóm hàng";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(175, 217);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(94, 217);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(13, 217);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Thêm mới";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(16, 154);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(234, 44);
+            this.txtDescription.TabIndex = 6;
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Location = new System.Drawing.Point(16, 106);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(234, 20);
+            this.txtCategoryName.TabIndex = 5;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(16, 58);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(234, 20);
+            this.txtId.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mã ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Mô tả";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tên Nhóm hàng";
+            // 
+            // FormCategoryManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 411);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormCategoryManagement";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý Danh mục Nhóm hàng";
+            this.Load += new System.EventHandler(this.FormCategoryManagement_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategories)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvCategories;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ MiniSupermarketWinForms/FormCategoryManagement.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniSupermarketWinForms
+{
+    public partial class FormCategoryManagement : Form
+    {
+        // Địa chỉ Web API
+        // PHẢI sửa port cho đúng với Swagger của bạn
+        private static readonly HttpClient _client = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:7006/api/")
+        };
+
+        public FormCategoryManagement()
+        {
+            InitializeComponent();
+        }
+
+        // Khi Form mở lên
+        private async void FormCategoryManagement_Load(object sender, EventArgs e)
+        {
+            await LoadDataAsync();
+        }
+
+        // =========================================================
+        // LOAD DANH SÁCH
+        // GET /api/categories
+        // =========================================================
+        private async Task LoadDataAsync()
+        {
+            try
+            {
+                var categories =
+                    await _client.GetFromJsonAsync<List<CategoryDto>>("categories");
+
+                dgvCategories.DataSource = categories;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Không thể kết nối đến Server!\n\n" + ex.Message,
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        // =========================================================
+        // CLICK VÀO DÒNG TRÊN DATAGRIDVIEW
+        // =========================================================
+        private void dgvCategories_CellClick(
+            object sender,
+            DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            DataGridViewRow row = dgvCategories.Rows[e.RowIndex];
+
+            txtId.Text =
+                row.Cells["CategoryId"].Value?.ToString() ?? "";
+
+            txtCategoryName.Text =
+                row.Cells["CategoryName"].Value?.ToString() ?? "";
+
+            txtDescription.Text =
+                row.Cells["Description"].Value?.ToString() ?? "";
+        }
+
+        // =========================================================
+        // NÚT TẢI LẠI
+        // =========================================================
+        private async void btnLoad_Click(object sender, EventArgs e)
+        {
+            await LoadDataAsync();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        // =========================================================
+        // THÊM MỚI
+        // POST /api/categories
+        // =========================================================
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCategoryName.Text))
+            {
+                MessageBox.Show(
+                    "Vui lòng nhập tên nhóm hàng!",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            try
+            {
+                var newCategory = new
+                {
+                    CategoryName = txtCategoryName.Text.Trim(),
+                    Description = txtDescription.Text.Trim()
+                };
+
+                var response =
+                    await _client.PostAsJsonAsync(
+                        "categories",
+                        newCategory);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show(
+                        "Thêm nhóm hàng thành công!",
+                        "Thông báo",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
+                    await LoadDataAsync();
+
+                    ClearInputs();
+                }
+                else
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+
+                    MessageBox.Show(
+                        "Thêm thất bại!\n\n" + message,
+                        "Lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Có lỗi xảy ra:\n\n" + ex.Message,
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        // =========================================================
+        // CẬP NHẬT
+        // PUT /api/categories/{id}
+        // =========================================================
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtId.Text, out int id))
+            {
+                MessageBox.Show(
+                    "Vui lòng chọn nhóm hàng cần cập nhật!",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtCategoryName.Text))
+            {
+                MessageBox.Show(
+                    "Tên nhóm hàng không được để trống!",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            try
+            {
+                var updateCategory = new
+                {
+                    CategoryId = id,
+                    CategoryName = txtCategoryName.Text.Trim(),
+                    Description = txtDescription.Text.Trim()
+                };
+
+                var response =
+                    await _client.PutAsJsonAsync(
+                        $"categories/{id}",
+                        updateCategory);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show(
+                        "Cập nhật thành công!",
+                        "Thông báo",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
+                    await LoadDataAsync();
+
+                    ClearInputs();
+                }
+                else
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+
+                    MessageBox.Show(
+                        "Cập nhật thất bại!\n\n" + message,
+                        "Lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Có lỗi xảy ra:\n\n" + ex.Message,
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        // =========================================================
+        // XÓA
+        // DELETE /api/categories/{id}
+        // =========================================================
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtId.Text, out int id))
+            {
+                MessageBox.Show(
+                    "Vui lòng chọn nhóm hàng cần xóa!",
+                    "Cảnh báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            DialogResult confirm = MessageBox.Show(
+                $"Bạn có chắc muốn xóa nhóm hàng ID = {id}?",
+                "Xác nhận xóa",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (confirm != DialogResult.Yes)
+                return;
+
+            try
+            {
+                var response =
+                    await _client.DeleteAsync($"categories/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show(
+                        "Xóa thành công!",
+                        "Thông báo",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
+                    await LoadDataAsync();
+
+                    ClearInputs();
+                }
+                else
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+
+                    MessageBox.Show(
+                        "Xóa thất bại!\n\n" + message,
+                        "Lỗi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Có lỗi xảy ra:\n\n" + ex.Message,
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        // =========================================================
+        // TÌM KIẾM
+        // GET /api/categories/search?keyword=...
+        // =========================================================
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtKeyword.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                await LoadDataAsync();
+                return;
+            }
+
+            try
+            {
+                string url =
+                    $"categories/search?keyword={Uri.EscapeDataString(keyword)}";
+
+                var result =
+                    await _client.GetFromJsonAsync<List<CategoryDto>>(url);
+
+                dgvCategories.DataSource = result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Không thể tìm kiếm!\n\n" + ex.Message,
+                    "Lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        // =========================================================
+        // XÓA TRẮNG FORM
+        // =========================================================
+        private void ClearInputs()
+        {
+            txtId.Clear();
+            txtCategoryName.Clear();
+            txtDescription.Clear();
+        }
+    }
+
+    // DTO nhận dữ liệu từ API
+    public class CategoryDto
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = string.Empty;
+
+        public string Description { get; set; }
+    }
+}
